7436605e4b91d595116a4e939f0444e9d974e352
Alex Griep <alexgriep@hotmail.com>
2020-01-28 00:19:38 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-var express = require("express");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+app.use(logger("dev"));
 
-var db = require ("./models");
+// var db = require ("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,6 +15,12 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Connect to Mongo DB with Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -24,8 +33,6 @@ app.set("view engine", "handlebars");
 require("./controllers/")(app);
 
 // Start our server so that it can begin listening to client requests.
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    })
-});
+app.listen(PORT, () => {
+    console.log(`Application running on PORT ${PORT}`);
+  });
